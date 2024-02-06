a48ec799276fdc102ae245105339d2b868043720
Grace Lemming <lemming.9@osu.edu>
2024-02-06 16:39:55 -0500

    hyperlinked

--- app.py
+++ app.py
@@ -2,17 +2,11 @@ from flask import Flask, render_template, request
 import requests
 from elasticsearch import Elasticsearch
 
-
-# enter in command line 'flask run'
-# look up this url: http://127.0.0.1:5000/
-
 app = Flask(__name__)
 
 @app.route("/") #default temp
 def index():
     return render_template('index.html')
-
-
 
 @app.route("/search", methods=['GET', 'POST']) #format, fetch then add to html
 def search():
@@ -20,13 +14,22 @@ def search():
     ingredients = ingredients.split(',')
     apiKey = '96ef38777c94480b8b5e59393bac8bca'
     url = 'https://api.spoonacular.com/recipes/findByIngredients'
-    params = {'ingredients' : ingredients, 
-              'number' : 5,
-              'apiKey' : apiKey
+    params = {'ingredients': ingredients, 
+              'number': 5,
+              'apiKey': apiKey
               }
-    recipes = requests.get(url = url, params = params)
+    recipes = requests.get(url=url, params=params)
     recipes = recipes.json()
     if recipes:
         return render_template('index.html', recipes=recipes)
     else:
         return render_template('index.html')
+
+@app.route('/recipe/<int:recipe_id>') # dynamic URL to handle individual recipes
+def recipe(recipe_id):
+    apiKey = '96ef38777c94480b8b5e59393bac8bca'
+    url = f'https://api.spoonacular.com/recipes/{recipe_id}/information'
+    params = {'apiKey': apiKey}
+    recipe_details = requests.get(url=url, params=params)
+    recipe_details = recipe_details.json()
+    return render_template('recipe.html', recipe=recipe_details)
