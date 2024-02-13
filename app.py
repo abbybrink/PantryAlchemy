dc7e85b57e12f85a980e7c1e898a48f86c5f2ae5
hom-john <homjohn00@gmail.com>
2024-02-13 17:02:18 -0500

    changes in my branch

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request
 import requests
 from elasticsearch import Elasticsearch
-
-from dotenv import load_dotenv
-load_dotenv()
+import json
+# from dotenv import load_dotenv
+# load_dotenv()
 
 app = Flask(__name__)
+es = Elasticsearch('https://localhost:9200', ca_certs='http_ca.crt', basic_auth=("elastic", "=Q0LSPrqauirlURpWZHO"))
+
 
 @app.route("/") #default temp
 def index():
@@ -18,13 +20,35 @@ def search():
     apiKey = '96ef38777c94480b8b5e59393bac8bca'
     url = 'https://api.spoonacular.com/recipes/findByIngredients'
     params = {'ingredients': ingredients,
-              'number': 5,
+              'number': 100,
               'apiKey': apiKey
               }
     recipes = requests.get(url=url, params=params)
     recipes = recipes.json()
     if recipes:
-        return render_template('index.html', recipes=recipes)
+        with open("new.json", "w") as file:
+            json.dump(recipes, file, indent = 4)
+            #add index bullshit
+
+        if request.form['checkbox'].checked == True:
+            search_query = {
+                "query": {
+                    "match": {
+                        "missedIngredientCount": 0
+                    }
+                }
+            }
+        else:
+            search_query = {
+                "query": {
+                    "match": {
+                        "usedIngredientCount": ingredients.count
+                    }
+                }
+            }
+
+        results = es.search(index="", body=search_query)
+        return render_template('index.html', recipes=results)
     else:
         return render_template('index.html')
 
