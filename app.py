ccfb087fecd7adca2fbcd146506616728f29f712
dchristina2 <104952049+dchristina2@users.noreply.github.com>
2024-02-20 16:25:08 -0500

    added instructions for dependencies

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
+import pyrebase
 import os
 from flask import Flask, render_template, request
 import requests
 from elasticsearch import Elasticsearch
 
-
+from dotenv import load_dotenv
+load_dotenv()
+# Initialize Firebase app
+firebase_config = {
+    "apiKey": os.getenv("FIREBASE_API_KEY"),
+    "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
+    "databaseURL": os.getenv("FIREBASE_DATABASE_URL"),
+    "projectId": os.getenv("FIREBASE_PROJECT_ID"),
+    "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
+    "messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID"),
+    "appId": os.getenv("FIREBASE_APP_ID"),
+    "measurementId": os.getenv("FIREBASE_MEASUREMENT_ID")
+}
+firebase=pyrebase.initialize_app(firebase_config)
+auth=firebase.auth()
 
 app = Flask(__name__)
 
